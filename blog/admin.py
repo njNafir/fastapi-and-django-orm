0960e3c0d86e5b3d84c00991f716b45c69484057
NJ Nafir <njnafir@gmail.com>
2023-03-01 18:37:18 +0600

    Add Models, and Migrate

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,28 @@
-from django.contrib import admin
+from django.contrib import admin 
+from blog.models import Category, Post
 
-# Register your models here.
+
+class BlogCategoryAdmin(admin.ModelAdmin):
+    list_display: list = ['title', 'active']
+    list_display_links: list = ['title']
+    list_filter: list = ['updated', 'active']
+    search_fields: list = ['title']
+    list_editable: list = ['active']
+    list_per_page: list = 10
+    ordering: tuple = ('-id',)
+
+admin.site.register(Category, BlogCategoryAdmin)
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display: list = ['title', 'updated', 'created_on', 'publish', 'draft']
+    list_display_links: list = ['title']
+    list_filter: list = ['updated', 'created_on', 'publish']
+    list_editable: list = ['draft']
+    search_fields: list = ['title', 'content']
+    list_per_page: int = 10
+    ordering: tuple = ('-id',)
+
+class Meta:
+    model: Post = Post
+
+admin.site.register(Post, PostModelAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,74 @@
-from django.db import models
+from datetime import date, datetime 
+from pathlib import Path 
+from typing import Any, Callable, List, Union 
+from django.conf import settings 
+from django.db import models 
+from pydantic import AnyUrl 
+from blog.managers import CategoryManager, PostManager
 
-# Create your models here.
+def upload_image_path(instance: Any, filename: str) -> Union[str, Callable, Path]:
+    """
+    Set up the default thumbnail upload path.
+    """
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" %(instance.slug, ext)
+    return "blog_post_thumbnails/%s/%s" %(instance.slug, filename)
+
+class Category(models.Model):
+    """
+    Model for blog tags.
+    """
+    title: str = models.CharField(max_length=200)
+    description: str = models.CharField(max_length=200, blank=True, null=True)
+    slug: str = models.SlugField(unique=True, blank=True)
+    parent: Union[str, int, list] = models.ForeignKey('self', null=True, default=1, related_name='tags', on_delete=models.SET_DEFAULT)
+    active: bool = models.BooleanField(default=False)
+    updated: datetime = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created_on: datetime = models.DateTimeField(auto_now=False, auto_now_add=True)
+    objects: CategoryManager = CategoryManager()
+    
+    class Meta:
+        constraints: List[Any] = [models.UniqueConstraint(fields=['title', 'slug'], name='unique_blog_category'),]
+        verbose_name_plural: str = "categories"
+    
+    def __repr__(self) -> str:
+        return f"<class {self.slug}>"
+
+    def __str__(self) -> str:
+        return self.title
+
+class Post(models.Model):
+    """
+    Model for Blog Posts.
+    """
+    user: Any = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, null=True, on_delete=models.SET_DEFAULT, related_name="posts")
+    category: Any = models.ForeignKey(Category, null=True, default=1, blank=True, on_delete=models.SET_DEFAULT, related_name='post_category')
+    title: str = models.CharField(max_length=250)
+    description: str = models.TextField(null=True, blank=True)
+    content: str = models.TextField(default="Create a post.")
+    slug: str = models.SlugField(unique=True, blank=True)
+    thumbnail: Union[AnyUrl, str] = models.FileField(upload_to=upload_image_path, null=True, blank=True, max_length=1000)
+    draft: bool = models.BooleanField(default=False)
+    publish: date = models.DateField(auto_now=False, auto_now_add=False)
+    read_time: int = models.IntegerField(default=0)
+    view_count:int = models.PositiveIntegerField(default=0)
+    updated: datetime = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created_on: datetime = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    objects: PostManager = PostManager()
+
+    class Meta:
+        verbose_name: str = "post"
+        verbose_name_plural: str = "posts"
+        ordering: list = ["-publish", "title"]
+
+    def __repr__(self) -> str:
+        return "<Post %r>" % self.title
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+    def get_thumbnail_url(self) -> Union[Path, str, AnyUrl]:
+        timestamp = "%s%s%s%s%s" % (datetime.now().year, datetime.now().day, datetime.now().hour, datetime.now().minute, datetime.now().second)
+        if self.thumbnail and hasattr(self.thumbnail, 'url'):
+            return "%s?enc=%s" % (self.thumbnail.url, timestamp)
